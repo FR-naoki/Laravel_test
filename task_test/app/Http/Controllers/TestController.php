e40e20ae067deaa8981fe653ffad924f27f55ec2
FR_naoki <naoki.maekawa.511@gmail.com>
2024-09-25 21:24:52 +0900

    9/25

--- task_test/app/Http/Controllers/TestController.php
+++ task_test/app/Http/Controllers/TestController.php
@@ -4,17 +4,32 @@ namespace App\Http\Controllers;
 
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Test;
-
 
 class TestController extends Controller
 {
     public function index()
     {
+        //Eloquent（エロクアント）
         $values = Test::all();
 
-        // dd($values);
+        $count = Test::count();
 
-        return view('tests.test');
+        $first = Test::findOrFail(1);
+
+        $whereBBB = Test::where('text', '=', 'aaa')->get();
+
+        // dd($values, $count, $first, $whereBBB);
+
+        // クエリビルダ
+        $queryBuilder = DB::table('tests')->where('text', '=', 'aaa')
+            ->select('id', 'text')
+            ->get();
+
+        dd($values, $count, $first, $whereBBB, $queryBuilder);
+
+
+        return view('tests.test', compact('values'));
     }
 }
